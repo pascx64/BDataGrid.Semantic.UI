4dbcab49737c1e2975615d4bb9378add7525cbdc
pascal.arnold <pascal.arnold@genikinc.com>
2020-04-20 11:06:26 -0400

    1.0.12-alpha

--- BDataGrid.Semantic.UI/SemanticStyle.cs
+++ BDataGrid.Semantic.UI/SemanticStyle.cs
@@ -7,11 +7,23 @@ namespace BDataGrid.Semantic.UI
     public class SemanticStyle : Library.IBDataGridStyle
     {
         public string TableClass { get; set; } = "ui celled structured table";
+
         public string RowClass { get; set; } = "";
+
         public string CellClass { get; set; } = "";
+
         public string HeaderClass { get; set; } = "";
+
         public string HeaderRowClass { get; set; } = "";
 
         public string SelectedCell { get; set; } = "selectedCell";
+
+        public string PaginationDivClass { get; set; } = "ui right floated pagination menu";
+
+        public string PaginationPageNumberClass { get; set; } = "icon item";
+
+        public string PaginationLeftIcon { get; set; } = "left chevron icon";
+
+        public string PaginationRightIcon { get; set; } = "right chevron icon";
     }
 }
